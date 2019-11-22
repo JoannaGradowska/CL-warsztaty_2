dd5d68fd7c5569d7078ca1a933295ee6cebebf71
Joanna Gradowska <joanna.gradowska@icloud.com>
2019-11-22 23:09:59 +0100

    connecting to db, saving users

---
+++ connect.py
@@ -0,0 +1,21 @@
+from psycopg2 import connect, OperationalError
+from models.User import User
+from local_settings import user, password, host, database
+
+
+def connecting():
+    try:
+        cnx = connect(user=user, password=password, host=host, database=database)
+    except OperationalError as e:
+        return f"Nie udało się ustanowić połączenia ({e})"
+    else:
+        cnx.autocommit = True
+        cursor = cnx.cursor()
+        return cursor
+
+
+example = User()
+example.username = "example_name"
+example.email = "example@mail.pl"
+example.set_password("example_password")
+example.save_to_db(connecting())
---
+++ models/User.py
@@ -0,0 +1,34 @@
+from hash import password_hash
+
+class User(object):
+    __id = None
+    username = None
+    __hashed_password = None
+    email = None
+
+    def __init__(self):
+        self.__id = -1
+        self.username = ""
+        self.email = ""
+        self.__hashed_password = ""
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def hashed_password(self):
+        return self.__hashed_password
+
+    def set_password(self, password, salt=None):
+        self.__hashed_password = password_hash(password, salt)
+
+    def save_to_db(self, cursor):
+        if self.__id == -1:
+            sql = """INSERT INTO Users(username, email, hashed_password)
+                     VALUES(%s, %s, %s) RETURNING id"""
+            values = (self.username, self.email, self.hashed_password)
+            cursor.execute(sql, values)
+            self.__id = cursor.fetchone()[0]
+            return True
+        return False
