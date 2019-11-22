61f7aa62de277a26192744a597b2c8b945d58962
Joanna Gradowska <joanna.gradowska@icloud.com>
2019-11-22 23:35:09 +0100

    loading and modifying existing users

--- models/User.py
+++ models/User.py
@@ -31,4 +31,39 @@ class User(object):
             cursor.execute(sql, values)
             self.__id = cursor.fetchone()[0]
             return True
+        else:
+            sql = """UPDATE Users SET username=%s, email=%s, hashed_password=%s
+                    WHERE id=%s"""
+            values = (self.username, self.email, self.hashed_password, self.id)
+            cursor.execute(sql, values)
+            return True
         return False
+
+    @staticmethod
+    def load_user_by_id(cursor, user_id):
+        sql = "SELECT id, username, email, hashed_password FROM users WHERE id=%s"
+        cursor.execute(sql, (user_id,))  # (user_id, ) - bo tworzymy krotkę
+        data = cursor.fetchone()
+        if data:
+            loaded_user = User()
+            loaded_user.__id = data[0]
+            loaded_user.username = data[1]
+            loaded_user.email = data[2]
+            loaded_user.__hashed_password = data[3]
+            return loaded_user
+        else:
+            return None
+
+    @staticmethod
+    def load_all_users(cursor):
+        sql = "SELECT id, username, email, hashed_password FROM Users"
+        ret = []
+        cursor.execute(sql)
+        for row in cursor.fetchall():
+            loaded_user = User()
+            loaded_user.__id = row[0]
+            loaded_user.username = row[1]
+            loaded_user.email = row[2]
+            loaded_user.__hashed_password = row[3]
+            ret.append(loaded_user)
+        return ret
